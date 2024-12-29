7877fda97a258b8e401e2edc606e329bbc3a31f6
tangbodi <tangbodi@gmail.com>
2024-12-29 16:34:18 -0500

    Update App.tsx
    updated

--- livestream-app/src/App.tsx
+++ livestream-app/src/App.tsx
@@ -1,21 +1,68 @@
 import {
-  LivestreamPlayer,
-  StreamVideo,
   StreamVideoClient,
+  StreamVideo,
   User,
+  StreamCall,
+  ParticipantView,
+  useCallStateHooks
 } from "@stream-io/video-react-sdk";
 
-const apiKey = "f4xufhd7rut4";
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJAc3RyZWFtLWlvL2Rhc2hib2FyZCIsImlhdCI6MTczNTQzODIzNCwiZXhwIjoxNzM1NTI0NjM0LCJ1c2VyX2lkIjoiIWFub24iLCJyb2xlIjoidmlld2VyIiwiY2FsbF9jaWRzIjpbImxpdmVzdHJlYW06bGl2ZXN0cmVhbV82ZTA3ODEzMi1hZDY1LTQ3NDMtODJlNi1mMzg2NDc4MGE3ZmEiXX0.TLNw9FBRQbEtKw14y7mWnNZXLtl2-9uKAtmDWx0kboc";
-const callId = "livestream_6e078132-ad65-4743-82e6-f3864780a7fa";
+const apiKey = "mmhfdzb5evj2";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0lHXzg4IiwidXNlcl9pZCI6IklHXzg4IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzU1MDU0NzksImV4cCI6MTczNjExMDI3OX0.v0fqaosVrM7i8-RvngmlzWvbbr8_vUkQWqNtHTTSIZI";
+const userId = "IG_88";
+const callId = "CjUbLrnO8BYF";
 
-const user: User = { type: "anonymous" };
+const user: User = { id: userId, name: "DummyNode" };
 const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call("livestream", callId);
+call.join({ create: true });
 
 export default function App() {
   return (
     <StreamVideo client={client}>
-      <LivestreamPlayer callType="livestream" callId={callId} />
+      <StreamCall call={call}>
+        <LivestreamView />
+      </StreamCall>
     </StreamVideo>
   );
 }
+
+const LivestreamView = () => {
+  const {
+    useCameraState,
+    useMicrophoneState,
+    useParticipantCount,
+    useIsCallLive,
+    useParticipants,
+  } = useCallStateHooks();
+
+  const { camera: cam, isEnabled: isCamEnabled } = useCameraState();
+  const { microphone: mic, isEnabled: isMicEnabled } = useMicrophoneState();
+  
+  const participantCount = useParticipantCount();
+  const isLive = useIsCallLive();
+
+  const [firstParticipant] = useParticipants();
+  
+  return (
+    <div style={{ display: "flex", flexDirection: 'column', gap: '4px' }}>
+      <div>{isLive ? `Live: ${participantCount}`: `In Backstage`}</div>
+      {firstParticipant ? (
+        <ParticipantView participant={firstParticipant} />
+      ) : (
+        <div>The host hasn't joined yet</div>
+      )}
+      <div style={{ display: 'flex', gap: '4px'}}>
+        <button onClick={() => (isLive ? call.stopLive() : call.goLive())}>
+          {isLive ? "Stop Live" : "Go Live"}
+        </button>
+        <button onClick={() => cam.toggle()}>
+          {isCamEnabled ? "Disable camera" : "Enable camera"}
+        </button>
+        <button onClick={() => mic.toggle()}>
+          {isMicEnabled ? "Mute Mic" : "Unmute Mic"}
+        </button>
+      </div>
+    </div>
+  );
+};
